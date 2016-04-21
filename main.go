f058eeb20be9e5854163da716fcf3721866748f2
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-04-21 16:32:11 +0600

    s/lorg/orgalorg/g

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-const usage = `lorg - gunter + guntalina for synchronizing files on many hosts.
+const usage = `orgalorg - synchronizing files on many hosts.
 
-lorg will create tar-archive from specified files, keeping file attributes and
-ownerships, then upload archive in parallel to the specified hosts and unpacks
-it in the temporary directory. No further actions will be done until all hosts
-unpacks the archive.
+orgalorg will create tar-archive from specified files, keeping file attributes
+and ownerships, then upload archive in parallel to the specified hosts and
+unpacks it in the temporary directory. No further actions will be done until
+all hosts unpacks the archive.
 
 Then, gunter will be launched with that temporary directory as templates source
 directory with empty data file (e.g. no template processing will be done).
@@ -33,8 +33,8 @@ Restrictions:
       authentication data used for all specified hosts;
 
 Usage:
-    lorg -h | --help
-    lorg [options] (-d <hosts-file-dir>|-f <hosts-file>|-o <host>)... -S <files>...
+    orgalorg -h | --help
+    orgalorg [options] (-d <hosts-file-dir>|-f <hosts-file>|-o <host>)... -S <files>...
 
 Required options:
     -o <host>            Target host in format [<username>@]<domain>[:<port>].
@@ -58,8 +58,8 @@ Options:
                            requested on stdin after program start.
                            Excludes '-i' option.
     -x                   Do not try to obtain root (via 'sudo -i').
-                         By default, lorg will try to obtain root and do all
-                           actions from root, because it's most common use
+                         By default, orgalorg will try to obtain root and do
+                             all actions from root, because it's most common use
                            case. To prevent that behaviour, this option can be
                            used.
     -v                   Print debug information on stderr.
@@ -68,14 +68,14 @@ Options:
 Advanced options:
     --backups-dir <dir>  Directory, that will be used on the remote hosts for
                            storing backups. Backups will be stored in the
-                           subdirectory, uniquely named with source hostname and
-                           timestamp.
+                           subdirectory, uniquely named with source hostname
+                           and timestamp.
                            This option is only useful with '-b', which is off
                            by default.
-                           [default: /var/lorg/backups/]
+                           [default: /var/orgalorg/backups/]
     --temp-dir <dir>     Use specified directory for storing temporary data
                            on each host.
-                           [default: /tmp/lorg/runs/]
+                           [default: /tmp/orgalorg/runs/]
 `
 
 func main() {
