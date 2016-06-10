f25d234f61c9f12d9c7cb02b984796f990fbccb6
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-06-10 13:01:38 +0600

    remove locks for lorg, looks like it thread safe

--- debug.go
+++ debug.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"sync"
-)
-
-var (
-	// wait until lorg will be thread-safe
-	loggerLock = sync.Mutex{}
-)
-
 func tracef(format string, args ...interface{}) {
-	loggerLock.Lock()
-	defer loggerLock.Unlock()
-
 	// TODO always write debug to the file
 	if verbose >= verbosityTrace {
 		logger.Debugf(format, args...)
@@ -20,23 +8,14 @@ func tracef(format string, args ...interface{}) {
 }
 
 func debugf(format string, args ...interface{}) {
-	loggerLock.Lock()
-	defer loggerLock.Unlock()
-
 	// TODO always write debug to the file
 	logger.Debugf(format, args...)
 }
 
 func infof(format string, args ...interface{}) {
-	loggerLock.Lock()
-	defer loggerLock.Unlock()
-
 	logger.Infof(format, args...)
 }
 
 func warningf(format string, args ...interface{}) {
-	loggerLock.Lock()
-	defer loggerLock.Unlock()
-
 	logger.Warningf(format, args...)
 }
