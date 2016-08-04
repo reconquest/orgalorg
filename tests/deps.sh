1e7bcfb2c85897041e76d2fc16f65ce8614426e0
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-08-04 13:24:07 +0700

    tests: add expect dependency test

--- tests/deps.sh
+++ tests/deps.sh
@@ -4,6 +4,11 @@
         exit 1
     fi >&2
 
+    if ! which expect >/dev/null 2>&1; then
+        echo "missing dependency: expect"
+        exit 1
+    fi >&2
+
     if ! which hastur >/dev/null 2>&1; then
         echo "missing dependency: hastur (https://github.com/seletskiy/hastur)"
         exit 1
