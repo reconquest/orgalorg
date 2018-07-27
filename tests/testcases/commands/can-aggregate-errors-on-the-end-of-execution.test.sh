fd7a577c0765b8b21aae8334cf0637ffd6309ccb
Stanislav Seletskiy <s.seletskiy@gmail.com>
2018-07-27 13:28:20 +0300

    fix tests for new versions

--- tests/testcases/commands/can-aggregate-errors-on-the-end-of-execution.test.sh
+++ tests/testcases/commands/can-aggregate-errors-on-the-end-of-execution.test.sh
@@ -1,3 +1,4 @@
 tests:not tests:ensure :orgalorg:with-key -e -C echo 1 '&&' exit 1
-tests:assert-stderr-re "exited with non-zero.*all $(containers:count) nodes"
+tests:assert-stderr-re "exited with non-zero"
+tests:assert-stderr-re "all $(containers:count) nodes"
 tests:assert-stderr "code 1 ($(containers:count) nodes)"
